77787c50cb9145096ec25c271b1adfd4c7c50bf9
David <darknight4123@gmail.com>
2024-01-25 21:52:52 +0200

    Added caching for WebClient instances
    Creating a new client per endpoint with the customer's token every time there's an alert - is stupid.

--- src/main/java/dte/tzevaadomtracker/webclient/WebClientFactory.java
+++ src/main/java/dte/tzevaadomtracker/webclient/WebClientFactory.java
@@ -2,10 +2,12 @@ package dte.tzevaadomtracker.webclient;
 
 import dte.tzevaadomtracker.alertendpoint.AlertEndpoint;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @FunctionalInterface
 public interface WebClientFactory
 {
+    @Cacheable("web-clients")
     WebClient getClientFor(AlertEndpoint endpoint);
 }
