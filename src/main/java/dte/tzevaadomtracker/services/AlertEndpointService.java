7971c49bf11a8ee8374bc29611d462f044bea5c3
David <darknight4123@gmail.com>
2024-02-01 22:51:39 +0200

    Endpoints objects are now cached on startup

--- src/main/java/dte/tzevaadomtracker/services/AlertEndpointService.java
+++ src/main/java/dte/tzevaadomtracker/services/AlertEndpointService.java
@@ -6,6 +6,7 @@ import dte.tzevaadomtracker.events.TzevaAdomEvent;
 import dte.tzevaadomtracker.repositories.AlertEndpointRepository;
 import dte.tzevaadomtracker.webclient.WebClientFactory;
 
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;;
 import java.util.List;
 
 @Service
@@ -20,6 +22,8 @@ public class AlertEndpointService
 {
     private final AlertEndpointRepository alertEndpointRepository;
     private final WebClientFactory webClientFactory;
+
+    private final List<AlertEndpoint> endpoints = new ArrayList<>();
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AlertEndpointService.class);
 
@@ -29,18 +33,25 @@ public class AlertEndpointService
         this.webClientFactory = webClientFactory;
     }
 
+    @PostConstruct
+    public void loadAllEndpoints()
+    {
+        this.endpoints.addAll(this.alertEndpointRepository.findAll());
+
+        LOGGER.info("Loaded {} endpoints from the database.", this.endpoints.size());
+    }
+
     @Async
     @EventListener
     public void onTzevaAdom(TzevaAdomEvent event)
     {
         Alert alert = event.getSource();
-        List<AlertEndpoint> usersEndpoints = this.alertEndpointRepository.findAll();
 
         //log the Tzeva Adom
         LOGGER.info("Notifying {} server(s) about a Tzeva Adom in {}!", this.alertEndpointRepository.count(), alert.getRegion());
 
         //notify the users' servers
-        usersEndpoints.forEach(server -> notifyTzevaAdom(server, alert));
+        this.endpoints.forEach(server -> notifyTzevaAdom(server, alert));
     }
 
     public void notifyTzevaAdom(AlertEndpoint endpoint, Alert alert)
